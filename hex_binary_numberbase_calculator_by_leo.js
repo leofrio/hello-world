290e42d07c1f643cdf29eca5506fd831f443f963
Leonardo Pontes Gaspar <49174917+leofrio@users.noreply.github.com>
2019-04-10 11:04:10 -0300

    Update hex_binary_numberbase_calculator_by_leo.js

--- hex_binary_numberbase_calculator_by_leo.js
+++ hex_binary_numberbase_calculator_by_leo.js
@@ -5,22 +5,48 @@ function show(text) {
 	document.write(text) 
 	skip()
 } 
+function A(al) { 
+	alert(al)
+} 
 var want=prompt("do you want to have your number calculated in 1-binary 2-hexadecimal 3- a base you decide;type only the number or the word with no capitals") 
 
-if(want == "1" || want == "binary") {
-	a=[] 
-	var i = 0 
-	var arnaldo; 
+if(want == "1" || want == "binary") { 
 	var number=parseInt(prompt("what is the number you want to pass to binary"))
-	for(var c=number ; c > 0; c=Math.floor(c/2)) { 
-		arnaldo=c % 2 
-		a[i]=arnaldo 
-		i++
-	}  
-	document.write(number +  " in binary equals:")
-	for(var b = i-1 ; b >= 0; b--) { 
-		document.write(a[b])
-	} 
+	if(number >= 0) {
+		a=[] 
+		var i = 0 
+		var arnaldo; 
+		for(var c=number ; c > 0; c=Math.floor(c/2)) { 
+			arnaldo=c % 2 
+			a[i]=arnaldo 
+			i++
+		}  
+		document.write(number +  " in binary equals:")
+		for(var b = i-1 ; b >= 0; b--) { 
+			document.write(a[b])
+		} 
+ 	} 
+	if(number <  0) { 
+		A("since the number you want is negaative, we will have to use the binary signed 2 format")
+		a=[] 
+		var i = 0 
+		var arnaldo; 
+		for(var c=number ; c > 0; c=Math.floor(c/2)) { 
+			arnaldo=c % 2 
+			if(arnaldo == 1) { 
+				arnaldo = 0
+			} 
+			if(arnaldo == 0) { 
+				arnaldo = 1
+			} 
+			a[i]=arnaldo  
+			i++
+		}  
+		document.write(number +  " in binary signed 2 form equals:")
+		for(var b = i-1 ; b >= 0; b--) { 
+			document.write(a[b])
+		}
+	}
 } 
 if(want == "2" || want == "hexadecimal") {
 	a=[] 
