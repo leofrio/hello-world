1a8add7a054a01c3ce951b99545997a5fcb787ca
Leonardo Pontes Gaspar <49174917+leofrio@users.noreply.github.com>
2019-09-04 02:45:33 -0700

    Update Setor.java

--- Java/poo/trabalhoav1leo/Setor.java
+++ Java/poo/trabalhoav1leo/Setor.java
@@ -1,12 +1,30 @@
+package well;
 
 public class Setor {
+	private String nome;
 	private int qtdfunc; 
 	private Funcionario[] func;
 	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	public int getQtdfunc() {
 		return qtdfunc;
 	}
 	public void setQtdfunc(int qtdfunc) {
 		this.qtdfunc = qtdfunc;
 	}
+	public Funcionario[] getFunc() {
+		return func;
+	}
+	public void setFunc(Funcionario[] func) {
+		this.func = func;
+	} 
+	
+	
 }
